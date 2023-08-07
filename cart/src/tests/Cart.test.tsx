28b8ababaf8fe751412425dc6d996be0fd056041
Paul Kogan <paulkog@gmail.com>
2023-08-07 16:21:44 -0400

    util test ok, fixing jest configs

--- cart/src/tests/Cart.test.tsx
+++ cart/src/tests/Cart.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
+import LoginPage from '../components/shared/LoginPage';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<LoginPage />);
+  const linkElement = screen.getByText(/Please Login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
