0ddeea42b8351e386e3ad8bbf011409f271a54b1
Paul Kogan <paulkog@gmail.com>
2023-08-08 12:04:20 -0400

    migrations fixed

--- cart/src/hooks/SessionContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-// not used
-const SessionContext = React.createContext();
-
-export const useSessionContext = () => React.useContext(SessionContext);
-
-export default SessionContext;
